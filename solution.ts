3e8f90a0cb316684442e7d49b5ee76aaaa815b65
Ahbabuzzaman <160356472+ahbab-zaman@users.noreply.github.com>
2025-11-17 03:09:35 +0600

    done the problem solving

---
+++ solution.ts
@@ -0,0 +1,116 @@
+function formatValue(
+  value: string | number | boolean
+): string | number | boolean {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else if (typeof value === "boolean") {
+    return !value;
+  }
+  return value;
+}
+
+function getLength(value: string | any[]): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  }
+
+  return 0;
+}
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+
+type Item = {
+  title: string;
+  rating: number;
+};
+function filterByRating(items: Item[]): Item[] {
+  return items.filter((item) => item.rating >= 4);
+}
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function filterActiveUsers(users: User[]): User[] {
+  return users.filter((user) => user.isActive === true);
+}
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): void {
+  const availability = book.isAvailable ? "Yes" : "No";
+  console.log(
+    `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${availability}`
+  );
+}
+
+function getUniqueValues(
+  array1: (string | number)[],
+  array2: (string | number)[]
+): (string | number)[] {
+  const combined: (string | number)[] = [];
+  const result: (string | number)[] = [];
+  for (let i = 0; i < array1.length; i++) {
+    combined[combined.length] = array1[i];
+  }
+  for (let i = 0; i < array2.length; i++) {
+    combined[combined.length] = array2[i];
+  }
+
+  for (let i = 0; i < combined.length; i++) {
+    let isDuplicate = false;
+    for (let j = 0; j < result.length; j++) {
+      if (combined[i] === result[j]) {
+        isDuplicate = true;
+        break;
+      }
+    }
+    if (!isDuplicate) {
+      result[result.length] = combined[i];
+    }
+  }
+
+  return result;
+}
+
+type Product = {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+};
+
+function calculateTotalPrice(products: Product[]): number {
+  if (products.length === 0) {
+    return 0;
+  }
+
+  return products.reduce((total, product) => {
+    return total + product.price * product.quantity;
+  }, 0);
+}
+
